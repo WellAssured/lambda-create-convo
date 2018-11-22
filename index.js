5e2aac7db0e0371e9d9b150e919f7f0d87f5ede7
Jack White <jwhite5672@gmail.com>
2018-11-22 11:22:55 -0500

    First commit to Lambda Create Convo.

--- index.js
+++ index.js
@@ -1,50 +1,85 @@
-'use strict';
-
-const axios = require('axios');
+global.fetch = require('node-fetch');
+const AmazonCognitoIdentity = require('amazon-cognito-identity-js');
 
 exports.handler = (event, context, callback) => {
-  const mailchimpKEY = process.env.api_key;
-  const mailchimpURL = process.env.api_url;
-  const json = JSON.parse(event.body);
-  // Post new subscriber to Mailchimp. (List # and API Key are stored in Lambda Environment Variables)
-  // a status of pending indicates that we expect Mailchimp to send a verification email to the user.
-  axios.post(mailchimpURL, {
-    email_address: json.email,
-    status: "subscribed",
-    merge_fields: {
-      FNAME: json.firstName,
-      LNAME: json.lastName,
+
+    if (event["Records"].length === 0) {
+        callback(null, event);
+        return; 
     }
-  },{
-    auth: {
-      username: 'username',
-      password: mailchimpKEY,
-    }
-  }).then(response =>
-    callback(null, {
-      "isBase64Encoded": false,
-      "statusCode": response.status,
-      "headers": { "Access-Control-Allow-Origin": '*'},
-      "body": JSON.stringify({'response': response.status, 'responseText': response.statusText})
-    })
-  ).catch(error => {
-    if (error.response) {
-      callback(null, {
-        "isBase64Encoded": false,
-        "statusCode": error.response.status,
-        "headers": { "Access-Control-Allow-Origin": '*'},
-        "body": JSON.stringify({
-          'errorResponseStatus': error.response.status,
-          'errorResponseData': error.response.data
-        })
-      });
-    } else {
-      callback({
-        "isBase64Encoded": false,
-        "statusCode": 500,
-        "headers": { "Access-Control-Allow-Origin": '*'},
-        "body": JSON.stringify({'response': error})
-      })
-    }
-  });
+    
+    // Log in via my Cognito
+    const authDetails = new AmazonCognitoIdentity.AuthenticationDetails({
+        Username: process.env.cognito_username,
+        Password: process.env.cognito_pw
+    });
+    const userPool = new AmazonCognitoIdentity.CognitoUserPool({
+        UserPoolId : event["Records"][0].messageAttributes.userPoolId.stringValue,
+        ClientId : event["Records"][0].messageAttributes.clientId.stringValue
+    });
+    const jacksCognito = new AmazonCognitoIdentity.CognitoUser({
+        Username: process.env.cognito_username,
+        Pool: userPool
+    });
+    
+    jacksCognito.authenticateUser(authDetails, {
+        onSuccess: result => {
+            const jwtToken = result.getAccessToken().getJwtToken();
+            const newUsers = {};
+            let user = {};
+            let message;
+            for (i in event["Records"]) {
+                message = event["Records"][i];
+                user = JSON.parse(message.body).newUser;
+                newUsers[user.name] = user;
+            }
+            
+            const newUserKeys = Object.keys(newUsers);
+            for (i in newUserKeys) {
+                const newUser = newUsers[newUserKeys[i]];
+                // Create a new conversation between the new User and Lora
+                // Create Conversation Mutation
+                const createConvo = `mutation createConvo($participants: [ID!]!) {
+                    createConversation(input: { participants: $participants }) { conversationId }
+                }`;
+                const participants = { participants: [newUser.sub, process.env.lora_sub] };
+                const welcomeText = `Hey there, ${newUser.name}! Welcome to TailRD Nutrition! Your personal Dietitian is named Lora and she's ready to chat. Feel free to get things started.`;
+                const sendWelcomeMessage = `mutation createMessage($convoId: ID!) {
+                    sendMessage(input: {
+                        conversationId: $convoId,
+                        content: "${welcomeText}"
+                    }) { timestamp }
+                }`;
+                fetch(process.env.gql_api_url, {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        query: createConvo,
+                        operationName: 'createConvo',
+                        variables: participants
+                    }),
+                    headers: {
+                        'Authorization': jwtToken,
+                        'Content-Type': 'application/json'
+                    }
+                }).then(res => res.json()).then(json => {
+                    const convoId = { convoId: json.data.createConversation.conversationId };
+                    // Send a welcome message to the new Conversation
+                    fetch(process.env.gql_api_url, {
+                        method: 'POST',
+                        body: JSON.stringify({
+                            query: sendWelcomeMessage,
+                            operationName: 'createMessage',
+                            variables: convoId
+                        }),
+                        headers: {
+                            'Authorization': jwtToken,
+                            'Content-Type': 'application/json'
+                        }
+                    }).then(res => res.json()).then(j => console.log(JSON.stringify(j))).catch(er => console.log(er, er.stack));
+                }).catch(e => console.log("Error Creating Conversation", e));
+            }
+        },
+        onFailure: err => console.log("AUTHFAIL", err)
+    });
+    callback(null, event);
 };
